1fee3167b5b6a290057570cdefd4f933569f0586
Marlon Gumban <msg2msg2@gmail.com>
2017-09-21 18:36:09 +0800

    added delete in database using express

--- controllers/drivers_controllers.js
+++ controllers/drivers_controllers.js
@@ -20,12 +20,20 @@ module.exports = {
     edit(req, res, next) {
         //the id at the end of req.params.id should match with
         //what you input at /api/driver/:id
-        const DriverId = req.params.id;
+        const driverId = req.params.id;
         const driverProps = req.body;
 
         Driver.findByIdAndUpdate({ _id: driverId, driverProps })
             .then(() => Driver.findById({ _id: driverId }))
-            .then(driver=> res.send(driver))
+            .then(driver => res.send(driver))
+            .catch(next);
+    },
+
+    delete(req, res, next) {
+        const driverId = req.params.id;
+
+        Driver.findByIdAndRemove({ _id: driverId })
+            .then(driver => res.status(204).send(driver))
             .catch(next);
     }
 };
--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -22,22 +22,38 @@ describe('Drivers controller', () => {
     });
 
     it('PUT to /api/drivers/id edits an existing driver', done => {
-        const driver = new Driver ({ email: 't@t.com', driving: false });
+        const driver = new Driver ({ email: 't@t.com', driving: true });
         driver.save().then(() => {
-            console.log(driver.driving);
             request(app)
             //ES5 you can use put('/api/drivers/' + driver._id)
             //ES6 or put(`/api/drivers/${driver._id}`)
                 .put(`/api/drivers/${driver._id}`)
-                .send({ driving: true })
+                .send({ driving: false })
                 .end(() => {
-                    console.log(driver.driving);
                     Driver.findOne({ email: 't@t.com' })
                         .then(driver => {
-                            assert(driver.driving === false);
+                            assert(driver.driving === true);
                             done();
                         });
                 });
         });
+    });
+
+    it('DELETE to /api/drivers/id deletes an existing driver', done => {
+        const driver = new Driver ({ email: 'test@test.com', driving: true });
+
+            driver.save().then(() => {
+                request(app)
+                //ES5 you can use put('/api/drivers/' + driver._id)
+                //ES6 or put(`/api/drivers/${driver._id}`)
+                    .delete(`/api/drivers/${driver._id}`)
+                    .end(() => {
+                        Driver.findOne({ email: 'test@test.com' })
+                        .then((driver) => {
+                            assert(driver === null);
+                            done();
+                        })
+                    });
+            });
     });
 });
